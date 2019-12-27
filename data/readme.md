dc7d6bb954eeff0a32512a76405120a588c90e9c
Lynn <firemagelynn@gmail.com>
2019-12-27 01:37:10 -0800

    checkpoint

---
+++ data/readme.md
@@ -0,0 +1,21 @@
+# ./data/*
+
+category folders containing stuff to be sorted, and their already sorted variants
+
+## structure
+
+```
+cat-breeds/
+  randomized.txt
+  sorted.txt
+  # no more files! just those two!
+dog-breeds/
+  randomized.txt
+  sorted.txt
+dinosaurs/
+  randomized.txt
+  sorted.txt
+# and so on...
+```
+
+in the example above, `cat-breeds` `dog-breeds` and `dinosaurs` are all "category folders".
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,24 @@
 set -euo pipefail
 set -o xtrace
 
-language="${1:-rust}"
+language="${1:-python}"
 
-for sortScript in ./src/$language/sort_*; do
+# for everything in the data folder
+for dataFile in ./data/*; do
+  # results come out looking like
+  #  ./data/first-names
+  #  ./data/readme.md
+  # etc, without including any folder contents
 
+  # except for readmes
+  if [[  $dataFile == "*/readme.md" ]]; then continue end
+
+  # sort them with every sort script
+  for sortScript in ./src/$language/sort_*; do
+    $language $sortScript $dataFile"/
+  done
+
+  if dataFile ==
+  echo $dataFile
+  # TODO
 done
