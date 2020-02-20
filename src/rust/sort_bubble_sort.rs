08ec3fcc57f89adc6d2511eb02c988ff3e0f8942
Lynn <firemagelynn@gmail.com>
2020-02-19 19:51:28 -0800

    broken bubble sort

--- src/rust/sort_bubble_sort.rs
+++ src/rust/sort_bubble_sort.rs
@@ -8,9 +8,39 @@ use std::io::Write;
 ///////////////////////
 
 fn do_sorting(input_list: Vec<&str>) -> Vec<&str> {
+    return bubble_sort(input_list);
+}
+
+fn bubble_sort(input_list: Vec<&str>) -> Vec<&str> {
     let mut output_list = input_list;
-    output_list.sort();
+    let mut is_sorted = false;
+
+    while is_sorted == false {
+        let (output_list, is_sorted) = do_sorting_round(output_list)
+    }
+
     return output_list;
+}
+
+fn do_sorting_round(input_list: Vec<&str>) -> (Vec<&str>, bool) {
+    let mut output_list = input_list;
+    let mut is_sorted = true;
+
+    for (index, element) in input_list.iter().enumerate() {
+        if (index == 0) {
+            output_list.append(element);
+        } else if element < output_list[index - 1] {
+            let previous_element = output_list[index - 1];
+            output_list.pop();
+            output_list.append(element);
+            output_list.append(previous_element);
+            is_sorted = false
+        } else {
+            output_list.append(element);
+        }
+    }
+
+    return (output_list, is_sorted)
 }
 
 /////////////////////
