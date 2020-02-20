a74dc18dae3459946aaf51ef5f027b82a1820c51
lynn [they] <lynncyrin@gmail.com>
2020-02-19 19:17:12 -0800

    Js bubble sort (#48)
    * not actually bubble sort yet
    
    * sort working!

--- src/js/sort_bubble_sort.js
+++ src/js/sort_bubble_sort.js
@@ -5,9 +5,39 @@ var fs = require('fs');
 ///////////////////////
 
 function doSorting(inputList) {
+  return bubbleSort(inputList);
+}
+
+function bubbleSort(inputList) {
   outputList = inputList;
-  outputList.sort();
-  return outputList;
+  isSorted = false;
+
+  while (isSorted === false) {
+    outputList, isSorted = doSortingRound(outputList);
+  }
+
+  return outputList
+}
+
+function doSortingRound(inputList) {
+  outputList = [];
+  isSorted = true;
+
+  inputList.forEach((element, index) => {
+    if (index === 0) {
+      outputList.push(element);
+    } else if (element < outputList[index - 1]) {
+      previousElement = outputList[index - 1];
+      outputList.pop();
+      outputList.push(element);
+      outputList.push(previousElement);
+      isSorted = false
+    } else {
+      outputList.push(element);
+    }
+  });
+
+  return outputList, isSorted;
 }
 
 /////////////////////
