ec44e10244c542b46c3a90ba0d1ea3dee31d350b
Lynn <firemagelynn@gmail.com>
2020-02-23 12:16:11 -0800

    go selection sort

---
+++ src/go/sort_selection_sort_test.go
@@ -0,0 +1,59 @@
+package algozone
+
+import (
+	"testing"
+)
+
+///////////////////////
+// sort script start //
+///////////////////////
+
+// selection sort!
+//
+// docs: https://en.wikipedia.org/wiki/Selection_sort
+//
+// Selection sort looks through every element an input list, and finds the
+// smallest element. That element is then appended to the end of an output list.
+// When it reaches the end of the input list, all of the output elements will
+// be sorted.
+
+func selectionSort(inputList []string) (sortedList []string) {
+	unsortedList := inputList
+	inputListLength := len(inputList)
+
+	for i := 0; i < inputListLength; i++ {
+		smallestIndex := findSmallestIndex(unsortedList)
+		smallestElement := unsortedList[smallestIndex]
+		sortedList = append(sortedList, smallestElement)
+		unsortedList = removeIndex(unsortedList, smallestIndex)
+	}
+
+	return sortedList
+}
+
+func findSmallestIndex(inputList []string) (smallestIndex int) {
+	for index, element := range inputList {
+		if element < inputList[smallestIndex] {
+			smallestIndex = index
+		}
+	}
+
+	return smallestIndex
+}
+
+func removeIndex(inputList []string, index int) (outputList []string) {
+	outputList = append(inputList[:index], inputList[index+1:]...)
+	return outputList
+}
+
+/////////////////////
+// sort script end //
+/////////////////////
+
+func TestSelectionSort(t *testing.T) {
+	inputList := getInputList()
+	outputList := selectionSort(inputList)
+	writeOutputList(outputList)
+}
+
+// ☝🏽 per-script helpers
