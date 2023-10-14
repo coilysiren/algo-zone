ad6341440596573238a9f5f254ae0d188025de65
Lynn <firemagelynn@gmail.com>
2023-10-14 07:17:45 -0400

    checkpint

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ import glob
 import os
 import subprocess
 import sys
+import pprint
 
 # 3rd party packages
 import yaml
@@ -41,16 +42,36 @@ class TestRunnerContext:
     script_output_file_name: str
     prepared_file_path: str
 
-    def __init__(self, language, script_path) -> None:
-        # setup our baseline configuration
-        data_folder_path = "./data"
-        base_directory = os.getcwd()
+    @property
+    def data(self):
+        return vars(self)
 
+
+class TestRunnerContexts:
+    base_directory = os.getcwd()
+    data_folder_path = "./data"
+    ctxs: list[TestRunnerContext] = []
+
+    def __init__(self, language) -> None:
         # get the language specific config
-        with open(f"{base_directory}/config.yml", "r", encoding="utf-8") as obj:
+        with open(f"{self.base_directory}/config.yml", "r", encoding="utf-8") as obj:
             data = obj.read()
             config_yaml = yaml.safe_load(data)
             config = config_yaml[language]
+
+        # generate the contexts
+        self.ctxs = []
+        for script_path in glob.glob(f"{self.base_directory}/src/{language}/sort_*"):
+            if ctx := self.generate(language, config, script_path):
+                self.ctxs.append(ctx)
+
+    @property
+    def data(self):
+        return [ctx.data for ctx in self.ctxs]
+
+    def generate(self, language, config, script_path):
+        # initialize a new context
+        ctx = TestRunnerContext()
 
         # given "src/python/sort_builtin.py" => split on "/" and return "sort_builtin.py"
         script_path_split_on_slash = script_path.split("/")
@@ -58,24 +79,24 @@ class TestRunnerContext:
 
         # given "sort_builtin.py" => split on "." and return "sort_builtin"
         script_name_split_on_dot = script_name_with_file_type.split(".")
-        self.script_name = script_name_split_on_dot[0]
+        ctx.script_name = script_name_split_on_dot[0]
 
         # given "sort_builtin" => return "sort"
-        script_type = self.script_name.split("_")[0]
+        script_type = ctx.script_name.split("_")[0]
 
         # this path is used in various places later
-        self.script_relative_path = f"./src/{language}/{script_name_with_file_type}"
+        ctx.script_relative_path = f"./src/{language}/{script_name_with_file_type}"
 
         # get the path of the file that's been prepared in advance
         # and has the output we would be expecting from out script
-        self.prepared_file_path = f"{data_folder_path}/{script_type}_output.txt"
+        ctx.prepared_file_path = f"{self.data_folder_path}/{script_type}_output.txt"
 
         # our scripts write their output files to this path
-        self.script_output_file_name = (
-            f"output_{script_type}_via_{language}_{self.script_name}.txt"
+        ctx.script_output_file_name = (
+            f"output_{script_type}_via_{language}_{ctx.script_name}.txt"
         )
-        self.script_output_file_path = (
-            f"{data_folder_path}/{self.script_output_file_name}"
+        ctx.script_output_file_path = (
+            f"{self.data_folder_path}/{ctx.script_output_file_name}"
         )
 
         # script_invoker is command that we run in a subprocess to invoke our script
@@ -88,31 +109,33 @@ class TestRunnerContext:
         # and that other invokers want script names (eg. script_name)
         # the useShortScriptName config value controls this behavior
         if config.get("useShortScriptName", False) is False:
-            script_to_invoke = self.script_relative_path
+            script_to_invoke = ctx.script_relative_path
         else:
-            script_to_invoke = self.script_name
+            script_to_invoke = ctx.script_name
 
         # construction initial call args
-        self.subprocess_args = [
+        ctx.subprocess_args = [
             "docker",
             "run",
-            f"--volume={base_directory}:/workdir",
+            f"--volume={self.base_directory}:/workdir",
             "-w=/workdir",
         ]
 
         # construct env vars CLI args
         if env_vars := config.get("env_vars"):
-            self.subprocess_args.append(f"-e={env_vars}")
+            ctx.subprocess_args.append(f"-e={env_vars}")
 
         # construct ending call args
-        self.subprocess_args += [
-            f"-e=INPUT_PATH={data_folder_path}/{script_type}_input.txt",
-            f"-e=OUTPUT_PATH={self.script_output_file_path}",
+        ctx.subprocess_args += [
+            f"-e=INPUT_PATH={self.data_folder_path}/{script_type}_input.txt",
+            f"-e=OUTPUT_PATH={ctx.script_output_file_path}",
             config["dockerImage"],
             *script_invoker,
             script_to_invoke,
             config.get("scriptSuffix", ""),
         ]
+
+        return ctx
 
 
 class TestRunner:
@@ -121,12 +144,10 @@ class TestRunner:
 
     def run_tests(self, language, input_script):
         # setup our baseline configuration
-        base_directory = os.getcwd()
+        test_runner_context = TestRunnerContexts(language)
 
-        # run every sort script
-        for script_path in glob.glob(f"{base_directory}/src/{language}/sort_*"):
-            ctx = TestRunnerContext(language, script_path)
-
+        # run every script
+        for ctx in test_runner_context.ctxs:
             # if an input script was passed in, and this script is not that input script
             # then continue on to the next script
             if inputs_are_truthy_and_different(
